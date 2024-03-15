405686237a97b90be52efd6115d73c3b7a80e0a3
Amana Samsudeen <2019is006@stu.ucsc.lk>
2024-03-15 23:06:29 +0530

    registration added

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,5 +10,24 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  constructor(private fb: FormBuilder) {}
+
+  signupForm = this.fb.group({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(30),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    contactno: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(
+        '^(?=.*\\d)(?=.*[A-Z])(?=.*?[#?!@$%^&*-])(?=.*[a-z])\\S{8,}$'
+      ),
+    ]),
+  });
+
   signup() {}
 }
