c4a8165191d2e753c654d932db3942a54ad7aec8
Umesha Sandamini <158459601+SandaminiDHU@users.noreply.github.com>
2024-04-06 20:14:39 +0530

    Feature/site content (#17)
    * site content
    
    * remove comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SigninPageComponent } from './components/signin-page/signin-page.compon
 import { SignupComponent } from './components/signup/signup.component';
 import { SysAdminTripCategoryComponent } from './components/trip-category/sys-admin-trip-category/sys-admin-trip-category.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { SysAdminSiteContentComponent } from './components/site-content/sys-admin-site-content/sys-admin-site-content.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
@@ -24,6 +25,10 @@ const routes: Routes = [
     path: 'sysadmin-category',
     component: SysAdminTripCategoryComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'site-content',
+    component: SysAdminSiteContentComponent,
   },
 
   { path: '**', redirectTo: '' },
